82431a53b95cafa1eb61c5a799904ba2cbcc5036
wkdewey@gmail.com <wkdewey@gmail.com>
2018-05-24 23:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var newkittens = kittens;
+  newkittens.push(name);
+  console.log(newkittens);
+  console.log(kittens)
+  return newkittens;
+}
+var kittens = ['Milo', 'Otis', 'Garfield']
+appendKitten('Broom')
